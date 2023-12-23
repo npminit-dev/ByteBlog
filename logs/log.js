5ff4a4903ab68fd75693024566f6ddab86ff7904
Jorge Antonio Balsamo <jorgeb.dev.acc@gmail.com>
2023-12-23 18:23:06 -0300

    Fixed bugs

---
+++ logs/log.js
@@ -0,0 +1,10 @@
+const { red, blue, green } = require('console-log-colors')
+require('dotenv').config()
+
+const log = {
+  info: (...params) => process.env.NODE_ENV==='development' ? console.log(blue(...params)) : null,
+  success: (...params) => process.env.NODE_ENV==='development' ?  console.log(green(...params)) : null,
+  error: (...params) => process.env.NODE_ENV==='development' ? console.log(red(...params)) : null
+}
+
+module.exports = log;
